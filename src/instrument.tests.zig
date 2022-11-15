fc526d8f577bae4072e0b865125bc8e4e8df8dde
huntrss <huntrss@posteo.me>
2022-11-15 23:41:05 +0100

    Started implementing for 0 and 1 argument functions. Added support for call conventions.

--- src/instrument.tests.zig
+++ src/instrument.tests.zig
@@ -7,25 +7,18 @@ fn zeroArgVoidFunction() void {
 
 const instrumentedZeroArgVoidFunction = instrument(zeroArgVoidFunction, "zero arg void function");
 
-fn zeroArgU8Function() u8 {
-    return 127;
-}
-
-const instrumentedZeroArgU8Function = instrument(zeroArgU8Function, "zero arg u8 function");
-
-inline fn inlinedZeroArgU8Function() u8 {
-    return 111;
-}
-
-const instrumentedInlinedZeroArgU8Function = instrument(inlinedZeroArgU8Function, "zero arg u8 function");
-
-
 test "Test instrument for void function with 0 Arguments" {
     const expect = @import("std").testing.expect;
 
     instrumentedZeroArgVoidFunction();
     try expect(true);
 }
+
+fn zeroArgU8Function() u8 {
+    return 127;
+}
+
+const instrumentedZeroArgU8Function = instrument(zeroArgU8Function, "zero arg u8 function");
 
 test "Test instrument for u8 function with 0 Arguments" {
     const expect = @import("std").testing.expect;
@@ -34,9 +27,57 @@ test "Test instrument for u8 function with 0 Arguments" {
     try expect(value == 127);
 }
 
+inline fn inlinedZeroArgU8Function() u8 {
+    return 111;
+}
+
+const instrumentedInlinedZeroArgU8Function = instrument(inlinedZeroArgU8Function, "zero arg u8 function");
+
 test "Test instrument for inlined u8 function with 0 Arguments" {
     const expect = @import("std").testing.expect;
 
     const value = instrumentedInlinedZeroArgU8Function();
     try expect(value == 111);
+}
+
+fn cCallconvOneArgi32Function(arg1: i32) callconv(.C) i32 {
+    return -12298 + arg1;
+}
+
+const instrumentedCCallConvOneArgi32Function = instrument(cCallconvOneArgi32Function, "cCallconvArgi32Function");
+
+test "Test instrument for C call convention i32 function with 1 Argument" {
+    const std = @import("std");
+    const expect = std.testing.expect;
+
+    const value = instrumentedCCallConvOneArgi32Function(8);
+    try expect(value == -12290);
+}
+
+fn oneAnytpeArgU8Function(arg1: anytype) u8 {
+    return arg1.a + arg1.b;
+}
+
+const instumentedOneAnytpeArgU8Function = instrument(oneAnytpeArgU8Function, "oneAnytpeArgU8Function");
+
+test "Test instrument for u8 function with 1 anytype Argument" {
+    const std = @import("std");
+    const expect = std.testing.expect;
+
+    const value = instumentedOneAnytpeArgU8Function(.{ .a = 5, .b = 7 });
+    try expect(value == 12);
+}
+
+fn oneComptimeArgU8SliceFunction(comptime T: type) []const u8 {
+    return @typeName(T);
+}
+
+const instumentedOneComptimeArgU8SliceFunction = instrument(oneComptimeArgU8SliceFunction, "oneComptimeArgU8SliceFunction");
+
+test "Test instrument for comptime T function with 1 comptime Argument" {
+    const std = @import("std");
+    const expect = std.testing.expect;
+
+    const value = instumentedOneComptimeArgU8SliceFunction(u8);
+    try expect(std.mem.eql(u8, value, "u8"));
 }
--- src/instrument.zig
+++ src/instrument.zig
@@ -2,16 +2,65 @@ const std = @import("std");
 const Span = @import("span.zig");
 
 pub inline fn instrument(comptime f: anytype, id: []const u8) @TypeOf(f) {
-    return instrument0Args(f, id);
+    const function_type = @typeInfo(@TypeOf(f)).Fn;
+    const return_type = function_type.return_type;
+    if (return_type == null) {
+        @compileError("Null return type is not supported");
+    }
+    const args = function_type.args;
+    const wrappedFunction = switch (args.len) {
+        0 => instrument0Args(f, id, function_type),
+        1 => instrument1Arg(f, id, function_type, args),
+        else => @compileError("Only up to 1 argument allowed. Use anytype or combined arguments in structs"),
+    };
+    return wrappedFunction;
 }
 
-inline fn instrument0Args(comptime f: anytype, id: []const u8) @TypeOf(f) {
+inline fn instrument0Args(comptime f: anytype, id: []const u8, comptime function_type: std.builtin.Type.Fn) @TypeOf(f) {
+    const calling_convention = function_type.calling_convention;
     const Wrapper = struct {
-        fn wrapped() @typeInfo(@TypeOf(f)).Fn.return_type.? {
+        fn wrapped() callconv(calling_convention) function_type.return_type.? {
             const span = Span.open(id);
             defer span.close();
             return f();
         }
     };
     return Wrapper.wrapped;
+}
+
+inline fn instrument1Arg(comptime f: anytype, id: []const u8, comptime function_type: std.builtin.Type.Fn, comptime args: []const std.builtin.Type.Fn.Param) @TypeOf(f) {
+    const calling_convention = function_type.calling_convention;
+    const arg = args[0];
+    if (arg.is_generic) {
+        const Wrapper = struct {
+            fn wrapped(p1: anytype) callconv(calling_convention) function_type.return_type.? {
+                const span = Span.open(id);
+                defer span.close();
+                return f(p1);
+            }
+        };
+        return Wrapper.wrapped;
+    } else {
+        const arg_type = arg.arg_type.?;
+        const arg_type_info = @typeInfo(arg_type);
+        if (arg_type_info == .Type) {
+            const Wrapper = struct {
+                fn wrapped(comptime p1: arg_type) callconv(calling_convention) function_type.return_type.? {
+                    const span = Span.open(id);
+                    defer span.close();
+                    return f(p1);
+                }
+            };
+            return Wrapper.wrapped;
+        } else {
+            const Wrapper = struct {
+                fn wrapped(p1: arg_type) callconv(calling_convention) function_type.return_type.? {
+                    const span = Span.open(id);
+                    defer span.close();
+                    return f(p1);
+                }
+            };
+            return Wrapper.wrapped;
+        }
+    }
 }
