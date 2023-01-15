dd99275e2f8f583a26f19da3a54c7f30fc81f7b2
huntrss <huntrss@posteo.me>
2023-01-15 23:30:17 +0100

    Finished multi-threading example

--- examples/multi_treading/src/main.zig
+++ examples/multi_treading/src/main.zig
@@ -10,24 +10,31 @@ fn threadFunction(msToSleep: u64) void {
     const span = Span.open("threadFunction");
     defer span.close();
     const id = Thread.getCurrentId();
-    std.debug.print("Hello from thread {}. About to sleep {} ms.\n", .{ id, msToSleep });
+    std.log.debug("Hello from thread {}. About to sleep {} ms.", .{ id, msToSleep });
     const nanoseconds = msToSleep * 1_000_000;
     sleep(nanoseconds);
-    std.debug.print("Thread {} ready sleeping {} ms.\n", .{ Thread.getCurrentId(), msToSleep });
+    std.log.debug("Thread {} ready sleeping {} ms.", .{ Thread.getCurrentId(), msToSleep });
 }
 
 pub fn main() !void {
     const span = Span.open("main");
     defer span.close();
-    std.debug.print("Using spans in a multi-threaded environment.\n", .{});
+    std.log.debug("Using spans in a multi-threaded environment.", .{});
 
-    const thread1 = try Thread.spawn(.{}, threadFunction, .{550});
-    sleep(50_000_000);
-    const thread2 = try Thread.spawn(.{}, threadFunction, .{200});
-    sleep(50_000_000);
-    const thread3 = try Thread.spawn(.{}, threadFunction, .{200});
-
+    // Threads interleave with each other
+    const thread1 = try Thread.spawn(.{}, threadFunction, .{50});
+    sleep(25_000_000);
+    const thread2 = try Thread.spawn(.{}, threadFunction, .{50});
     thread1.join();
     thread2.join();
+    std.log.debug("Interleaved threads example finished.", .{});
+
+    // Longer running thread  encloses shorter running thread
+    const thread3 = try Thread.spawn(.{}, threadFunction, .{100});
+    sleep(25_000_000);
+    const thread4 = try Thread.spawn(.{}, threadFunction, .{50});
+
     thread3.join();
+    thread4.join();
+    std.log.debug("Enclosing thread example finished.", .{});
 }
